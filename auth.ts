01a0f86647f9e9c3bc97d31e1c411d9f314c3d02
Mark Du <dulongjiebackup@gmail.com>
2024-01-19 14:15:55 -0500

    use callback, add token.sub as session user.id

--- auth.ts
+++ auth.ts
@@ -9,6 +9,17 @@ export const {
   auth,
   signIn,signOut
 } = NextAuth({
+  callbacks:{
+    async jwt({token}){      
+      return token
+    },
+    async session({token, session}){
+      if(token.sub && session.user){
+        session.user.id = token.sub;
+      }
+      return session
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig
